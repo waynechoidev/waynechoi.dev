5c55c8d304c154eadcc009e61615608b9de8ecc0
waynechoidev <wonjun92@gmail.com>
2023-10-20 23:57:34 +1300

    fix: menu margin

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -16,7 +16,7 @@ export default function MenuBar() {
       {menus.map((menu) => (
         <Link key={menu.label} href={menu.link} className="hover:text-blue-800">
           <span
-            className={`p-2 text-lg ${
+            className={`pl-3 text-lg ${
               pathname.includes(menu.link.replaceAll("/", ""))
                 ? "font-bold"
                 : "font-normal"
