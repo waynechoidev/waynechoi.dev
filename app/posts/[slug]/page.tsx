243a08af281d47396f52afdf5defc0ba1f42df0c
waynechoidev <wonjun92@gmail.com>
2023-10-20 14:15:37 +1300

    feat: add back button to post page

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,11 +1,16 @@
 import MarkdownRender from "@/components/MarkdownRender";
 import { getPostData, getPostList } from "@/service";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Post({ params }: { params: { slug: string } }) {
   const { title, date, tags, content } = getPostData(params.slug);
 
   return (
     <div>
+      <Link href="/posts" className="text-gray-400">
+        back
+      </Link>
       <h1>{title}</h1>
       <p>{date}</p>
       {tags.map((tag, index) => (
