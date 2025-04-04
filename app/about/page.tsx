f01463b282da57bbbb875b4005e0e1fa53a1336d
waynechoidev <wonjun92@gmail.com>
2025-04-04 22:50:34 +1300

    feat: update about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,21 +9,24 @@ export default function About() {
   return (
     <div className="max-w-profile_out mx-auto text-right">
       <div className="flex flex-col items-end max-w-profile_in ml-auto">
-        <h1>Hi!</h1>
-        <p className="my-2">I am Wayne, living in NZ.</p>
+        <h1 className="my-2">Hi!</h1>
+        <p className="my-2">I am Wayne, living in New Zealand.</p>
         <p className="my-2">
-          A frontend engineer with a passion for creating cutting-edge user
-          interfaces. Currently working for a fintech startup, and developing a
-          web-based CRM.
+          I am a software engineer with a passion for creating cutting-edge
+          software solutions. Currently working for a fintech startup,
+          developing a web-based CRM system.
         </p>
         <p className="my-2">
-          Love graphics programming, and enjoy working for both web and native
-          platforms. Strugling with mathematics, and computer science.
+          I love graphics programming and enjoy working on both web and native
+          platforms. I find joy in exploring advanced concepts in computer
+          graphics, and I am also deeply interested in computer vision,
+          exploring its applications and technologies.
         </p>
         <p className="my-2">
-          NZ qualified boat builder with 7 years past experience. Maybe one of
-          the most skilled software engineers nationwide when it comes to
-          building boats.
+          Before getting into software engineering, I was a qualified boat
+          builder in New Zealand with 7 years of experience, specializing in
+          high-performance boats, including RIBs for the Coast Guard and
+          Emirates Team New Zealand.
         </p>
         <div className="flex flex-row space-x-3 mt-10">
           {linkList.map((link) => (
